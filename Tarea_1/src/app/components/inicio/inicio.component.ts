d96904d529cc97c18a55cee8ad97062585e699df
RFerseg <ruben.fernandez@uned.cr>
2022-04-26 10:32:57 -0600

    Rubén

--- Tarea_1/src/app/components/inicio/inicio.component.ts
+++ Tarea_1/src/app/components/inicio/inicio.component.ts
@@ -8,11 +8,39 @@ import Swal from 'sweetalert2';
 })
 export class InicioComponent implements OnInit {
 
+  usuario: String = "";
+  pass: String = "";
+
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ValidarDatos(){
+    if(this.usuario == ""){
+      return "Digite el nombre de usuario";
+    }
+    if(this.pass == "" ){
+      return "Digite la contraseña";
+    }
+    if(this.usuario != 'admin' || this.pass != '1234'){
+      return "Usuario o Contraseña Incorrecta";
+    }
+    return "";
+  }
+
+  InicioSesion(){
+    let error = this.ValidarDatos();
+
+    if(error != ""){
+      Swal.fire('Error', error,'error');
+    }else{
+      Swal.fire('Bienvenido', 'Bienvenido ' + this.usuario,'success');
+    }
+    
+    
+  }
 
 
 }
